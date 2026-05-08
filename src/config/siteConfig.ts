aa0a8d3c17937fee3a994720232a62eb73d2e188
cloudinwind4132 <cloudinwind4132@outlook.com>
2026-05-09 01:56:13 +0800

    修改 firefly

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -75,7 +75,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "assets/images/cloudinwind_firefly.png",
+			value: "assets/images/firefly.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
